1819fd58d8c1803d7484ae56ebbef9ccb2c61d92
christian <delormechrist@gmail.com>
2021-07-03 15:51:27 +0200

    add scroll positon

--- src/router/index.js
+++ src/router/index.js
@@ -4,43 +4,51 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/contact',
-    name: 'Contact',
-    component: () => import('../views/Contact.vue')
-  },
-  {
-    path: '/homeLogo',
-    name: 'HomeLogo',
-    component: () => import('../views/HomeLogo.vue')
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
-  },
-  {
-    path: '/Skill',
-    name: 'Skill',
-    component: () => import('../views/Skill.vue')
-  },
-  {
-    path: '/projects',
-    name: 'Projects',
-    component: () => import('../views/Projects.vue')
-  },
-]
-
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: () => import('../views/Contact.vue')
+    },
+    {
+      path: '/homeLogo',
+      name: 'HomeLogo',
+      component: () => import('../views/HomeLogo.vue')
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: () => import('../views/About.vue')
+    },
+    {
+      path: '/Skill',
+      name: 'Skill',
+      component: () => import('../views/Skill.vue')
+    },
+    {
+      path: '/projects',
+      name: 'Projects',
+      component: () => import('../views/Projects.vue')
+    },
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
+ 
+ 
+  
 })
 
 export default router
