28742c2ab0850362d97e6e63002af05332f2884b
kriss <delormechrist@gmail.com>
2021-08-03 16:15:33 +0200

    .

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
-import Vue from 'vue'
-import * as VueGoogleMaps from 'vue2-google-maps'
-import App from './App.vue'
-import router from './router'
-import VueKinesis from 'vue-kinesis'
-import VueSweetalert2 from 'vue-sweetalert2';
+import Vue from "vue";
+import * as VueGoogleMaps from "vue2-google-maps";
+import App from "./App.vue";
+import router from "./router";
+import VueKinesis from "vue-kinesis";
+import VueSweetalert2 from "vue-sweetalert2";
 
 Vue.use(VueSweetalert2);
-Vue.use(VueKinesis)
-Vue.config.productionTip = false
+// Vue.use(VueKinesis)
+Vue.config.productionTip = false;
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyDvmZL_-Vr3zl4qvtmGFjBNRULS-e9C-Cw',
-    libraries: 'places',
-  }
+    key: "AIzaSyDvmZL_-Vr3zl4qvtmGFjBNRULS-e9C-Cw",
+    libraries: "places",
+  },
 });
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
